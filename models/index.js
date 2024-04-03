6a636eb283d1b460fb91cc598f0dc052be5b93cc
Ian Hart <ianhart7790@gmail.com>
2024-04-03 10:43:56 -0400

    feat: add model relationships file

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const Teacher = require('./Teacher');
+const Course = require('./Course');
+const Student = require('./Student');
+const StudentCourse = require('./StudentCourse');
+
+Teacher.hasMany(Course, {
+  foreignKey: 'teacher_id',
+  onDelete: 'CASCADE',
+});
+
+Course.belongsTo(Teacher, {
+  foreignKey: 'teacher_id',
+});
+
+Course.belongsToMany(Student, { through: StudentCourse, foreignKey: 'course_id' });
+Student.belongsToMany(Course, { through: StudentCourse, foreignKey: 'student_id' });
+
+module.exports = { Teacher, Course, Student, StudentCourse };

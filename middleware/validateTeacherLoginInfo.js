f4c640758ff352fa7ec89737eefd839f4aab6039
Ian Hart <ianhart7790@gmail.com>
2024-04-04 18:38:30 -0400

    chore: swap error and message keys

--- middleware/validateTeacherLoginInfo.js
+++ middleware/validateTeacherLoginInfo.js
@@ -1,7 +1,7 @@
 const validateTeacherLoginInfo = (req, res, next) => {
   const { email, password } = req.body;
   if (email.trim().length === 0 || password.trim().length === 0) {
-    return res.status(400).json({ message: 'Bad Request', error: 'Please make sure to fill out all fields' });
+    return res.status(400).json({ error: 'Bad Request', message: 'Please make sure to fill out all fields' });
   }
   next();
 };

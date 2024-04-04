463f15b27b7f199c9da326d0c93c78cc32584ce8
Ian Hart <ianhart7790@gmail.com>
2024-04-04 14:13:32 -0400

    feat: teacher can now logout

--- controllers/api/teacherRoutes.js
+++ controllers/api/teacherRoutes.js
@@ -62,4 +62,15 @@ router.post('/', validateCreateTeacher, async (req, res) => {
   }
 });
 
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;

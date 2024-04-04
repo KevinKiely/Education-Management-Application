8511ebf9219363fad17d3771f9505d6018281dec
Ian Hart <ianhart7790@gmail.com>
2024-04-04 13:51:29 -0400

    feat: register/create a teacher

--- controllers/api/teacherRoutes.js
+++ controllers/api/teacherRoutes.js
@@ -1,5 +1,30 @@
 const { Teacher } = require('../../models');
 const router = require('express').Router();
+const validateCreateTeacher = require('../../middleware/validateCreateTeacher');
+
+router.post('/', validateCreateTeacher, async (req, res) => {
+  try {
+    const existingTeacher = await Teacher.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (existingTeacher !== null) {
+      return res.status(400).json({ error: 'Bad Request', message: 'That email is already taken' });
+    }
+
+    const dbTeacherData = await Teacher.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = dbTeacherData.id;
+      req.session.logged_in = true;
+      req.session.name = dbTeacherData.name;
+      res.status(201).json({ message: 'Teacher created', data: {} });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error', message: error });
+  }
+});
 
 router.get('/', async (req, res) => {});
 

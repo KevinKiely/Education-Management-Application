31a6867221dc76bab500b496347c26248fd45d07
Ian Hart <ianhart7790@gmail.com>
2024-04-03 20:21:57 -0400

    feat: add course_description and update seeds

--- seeds/studentSeeds.js
+++ seeds/studentSeeds.js
@@ -1,16 +1,16 @@
 const { Student } = require('../models');
 
 const studentSeeds = [
-  { name: 'Emma Smith' },
-  { name: 'Liam Johnson' },
-  { name: 'Olivia Williams' },
-  { name: 'Noah Jones' },
-  { name: 'Ava Brown' },
-  { name: 'William Davis' },
-  { name: 'Sophia Miller' },
-  { name: 'James Wilson' },
-  { name: 'Isabella Moore' },
-  { name: 'Benjamin Taylor' },
+  { name: 'Emma Smith', grade_level: 9 },
+  { name: 'Liam Johnson', grade_level: 10 },
+  { name: 'Olivia Williams', grade_level: 11 },
+  { name: 'Noah Jones', grade_level: 12 },
+  { name: 'Ava Brown', grade_level: 9 },
+  { name: 'William Davis', grade_level: 10 },
+  { name: 'Sophia Miller', grade_level: 11 },
+  { name: 'James Wilson', grade_level: 12 },
+  { name: 'Isabella Moore', grade_level: 9 },
+  { name: 'Benjamin Taylor', grade_level: 10 },
 ];
 
 const seedStudents = () => Student.bulkCreate(studentSeeds, { returning: true });

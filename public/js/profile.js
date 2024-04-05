e82e808f075c4a1b8f66103831a8cdf6f4e5dde2
KevinKiely <Kevinkiely762@gmail.com>
2024-04-04 21:27:49 -0400

    Linked handlebars files to javascript, wrote functions for all of the buttons in the dashboard

---
+++ public/js/profile.js
@@ -0,0 +1,20 @@
+// Go to Homepage
+document.querySelector('#homepage').addEventListener("click", function(){
+    document.location.replace('/dashboard');
+    });
+    
+    // Go to Myclasses tab
+    document.querySelector('#myClasses').addEventListener('click', function(){
+    document.location.replace('/subjects');
+    });
+    
+    // Go to My profile
+    document.querySelector('#profile').addEventListener('click', function(){
+    document.location.replace('/profile');
+    });
+    
+    //Log out, redirects to login.handlebars
+    document.querySelector('#logout').addEventListener('click', function(){
+    document.location.replace('/login');
+    });
+    

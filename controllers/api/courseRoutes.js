922811764249460abaa7a0ae1937784f3dcabab6
Ian Hart <ianhart7790@gmail.com>
2024-04-04 20:45:28 -0400

    feat: add course routes starter file and pertaining middleware

--- controllers/api/courseRoutes.js
+++ controllers/api/courseRoutes.js
@@ -1,6 +1,21 @@
 const { Course } = require('../../models');
 const router = require('express').Router();
+const withAuth = require('../../middleware/withAuth');
+//const validateCreateCourse = require('../../middleware/validateCreateCourse');
 
-router.get('/', async (req, res) => {});
+router.post('/', withAuth, async (req, res) => {
+  try {
+    console.log(req.body);
+    //    await Course.create({
+    //      course_name: req.body.courseName,
+    //      course_description: req.body.courseDescription,
+    //      teacher_id: req.session.teacher_id,
+    //    });
+    //
+    res.status(201).json({ message: 'Course created' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error', message: error });
+  }
+});
 
 module.exports = router;

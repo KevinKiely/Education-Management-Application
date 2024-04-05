096f9d9cf4ea541ba63ffe0fd6225db93579a1d9
Ian Hart <ianhart7790@gmail.com>
2024-04-05 13:34:37 -0400

    feat: add route for getting all of a teacher's courses along with the students that belong to that course

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const withAuth = require('../middleware/withAuth');
+const { Course, Student, StudentCourse, Teacher } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
@@ -39,10 +40,24 @@ router.get('/dashboard', withAuth, (req, res) => {
   }
 });
 
-router.get('/courses', withAuth, (req, res) => {
+router.get('/courses', withAuth, async (req, res) => {
   try {
+    const dbCoursesData = await Course.findAll({
+      where: {
+        teacher_id: req.session.teacher_id,
+      },
+
+      include: [
+        { model: Teacher, attributes: ['id', 'name'] },
+        { model: Student, through: StudentCourse },
+      ],
+    });
+
+    const courses = dbCoursesData.map((course) => course.get({ plain: true }));
+
     res.render('subjects', {
       pageTitle: 'Subjects',
+      courses,
     });
   } catch (error) {
     res.status(500).json({ message: 'Internal Server Error', error });

04bc498696a62f6a119e9afcc583bec0284f76f2
Ian Hart <ianhart7790@gmail.com>
2024-04-08 21:26:50 -0400

    feat: change subject to courses in link

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,20 +1,20 @@
 // Go to Homepage
-document.querySelector('#homepage').addEventListener("click", function(){
-    document.location.replace('/dashboard');
-    });
-    
-    // Go to Myclasses tab
-    document.querySelector('#myClasses').addEventListener('click', function(){
-    document.location.replace('/subjects');
-    });
-    
-    // Go to My profile
-    document.querySelector('#profile').addEventListener('click', function(){
-    document.location.replace('/profile');
-    });
-    
-    //Log out, redirects to login.handlebars
-    document.querySelector('#logout').addEventListener('click', function(){
-    document.location.replace('/login');
-    });
-    
+document.querySelector('#homepage').addEventListener('click', function () {
+  document.location.replace('/dashboard');
+});
+
+// Go to Myclasses tab
+document.querySelector('#myClasses').addEventListener('click', function () {
+  document.location.replace('/courses');
+});
+
+// Go to My profile
+document.querySelector('#profile').addEventListener('click', function () {
+  document.location.replace('/profile');
+});
+
+//Log out, redirects to login.handlebars
+document.querySelector('#logout').addEventListener('click', function () {
+  document.location.replace('/login');
+});
+
